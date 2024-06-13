77429bb03a18c47238ead2c136ac00417fce6d56
Harlod Bombala <h.bombala@student.avans.nl>
2024-06-14 00:10:12 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioHarlod.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
