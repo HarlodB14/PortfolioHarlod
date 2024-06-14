d9124af932094423d0039d05127c90d9f1f5890f
Harlod Bombala <h.bombala@student.avans.nl>
2024-06-14 16:21:42 +0200

    added projects to homepage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PortfolioHarlod.Models;
+using PortfolioHarlod.Viewmodels;
 using System.Diagnostics;
 
 namespace PortfolioHarlod.Controllers
@@ -15,7 +16,16 @@ namespace PortfolioHarlod.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var blogItems = new List<BlogItemViewmodel>
+            {
+                new BlogItemViewmodel { title = "TreePainterProject", creationDate = DateTime.Now, description = "A JavaFX-Application where you can draw trees by adding them, let them move and save a picture", imageUrl = "Images/treepainter_image.png", Url ="https://github.com/HarlodB14/TreePainterProject"},
+                new BlogItemViewmodel { title = "Sagrada Group Project", creationDate = DateTime.Now, description = "A JavaFX-Application where you play Sagrada the game from floodgate games", imageUrl = "Images/Sagrada-inGame.png", Url = "https://github.com/HarlodB14/SagradaGame-Group-project"},
+                new BlogItemViewmodel { title = "ebay meets marktplaats meets IBood Project", creationDate = DateTime.Now, description = "A PHP/Laravel-application where I created a Ebay/Marktplaats like marketplace-system", imageUrl = "Images/Advertisements.png", Url = "https://github.com/HarlodB14/WEBPHP-bazaar"},
+                new BlogItemViewmodel { title = "Temple of Doom", creationDate = DateTime.Now, description = "A Duo-project where I contributed in. Based on the Indiana Jones movie, C#-console-game ", imageUrl = "Images/TempleOfDoom.png" , Url ="https://github.com/HarlodB14/TempleOfDoom"},
+            };
+
+            var viemodel = new BlogItemListViewmodel { blogItems = blogItems };
+            return View(viemodel);
         }
 
 
---
+++ Viewmodels/BlogItemViewmodel.cs
@@ -0,0 +1,17 @@
+﻿namespace PortfolioHarlod.Viewmodels
+{
+    public class BlogItemViewmodel
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string imageUrl { get; set; }
+        public string Url { get; set; }
+        public DateTime creationDate { get; set; }
+        public DateTime updatedDate { get; set; }
+    }
+
+    public class BlogItemListViewmodel
+    {
+        public List<BlogItemViewmodel> blogItems { get; set; }
+    }
+}
