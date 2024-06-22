74a3d88717bc1b02d02043b7628816bf10608697
Harlod Bombala <h.bombala@student.avans.nl>
2024-06-22 20:44:43 +0200

    removed CV

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -5,28 +5,6 @@ namespace PortfolioHarlod.Controllers
 {
     public class ContactController : Controller
     {
-        private readonly string _pdfFolderPath = "Document";
-        private readonly IWebHostEnvironment _hostingEnvironment;
-
-        public ContactController(IWebHostEnvironment hostEnvironment)
-        {
-            _hostingEnvironment = hostEnvironment;
-        }
-
-        public IActionResult DownloadCV()
-        {
-            string fileName = "CV-Harlod.pdf";
-            string webRootPath = _hostingEnvironment.WebRootPath;
-            string filePath = Path.Combine(webRootPath, _pdfFolderPath, fileName);
-
-            if (!System.IO.File.Exists(filePath))
-            {
-                return NotFound();
-            }
-
-            var fileBytes = System.IO.File.ReadAllBytes(filePath);
-            return File(fileBytes, "application/pdf", fileName);
-        }
 
         public IActionResult Contact()
         {
